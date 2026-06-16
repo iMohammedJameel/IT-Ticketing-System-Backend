d684c8bb2eb610deceb87ec9e9e0170f9546e186
Mohammed Jameel <107625539+iMohammedJameel@users.noreply.github.com>
2026-06-16 14:42:56 +0300

    Fix case of authMiddleware import path

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { addComment, getComments } = require("../controller/commentController");
-const { authMiddleware } = require("../Middleware/authMiddleware");
+const { authMiddleware } = require("../middleware/authMiddleware");
 
 router.post("/tickets/:ticketId/comments", authMiddleware, addComment);
 router.get("/tickets/:ticketId/comments", authMiddleware, getComments);
